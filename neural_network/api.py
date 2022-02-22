1af886cedd475d9c2809b5d36999505055881d20
gghendrickx <gghendrickx@gmail.com>
2022-02-22 10:51:21 +0100

    ADD: api.py: run web-API

---
+++ neural_network/api.py
@@ -0,0 +1,9 @@
+"""
+Online application to easily access the trained neural network.
+
+Author: Gijs G. Hendrickx
+"""
+import neural_network.application.app as app
+
+if __name__ == '__main__':
+    app.APP.run_server()
